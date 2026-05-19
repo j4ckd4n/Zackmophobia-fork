9346b91984503ad2b04d7189705e6a0b07eabf88
j4ckd4n <22923302+j4ckd4n@users.noreply.github.com>
2026-05-18 19:46:02 -0500

    fix: updates spacing to match set by IDE.

--- Hooks/GhostHooks.cpp
+++ Hooks/GhostHooks.cpp
@@ -68,40 +68,40 @@ void Hooks::hkGhostAI_Init(void* instance, GhostData_o* data, void* methodInfo)
 }
 
 void Hooks::hkGhostUpdate(void* instance, void* methodInfo) {
-     static bool logged = false;
-     if (!logged) {
-          Logger::Log("[HOOK] GhostInfo.Update is executing.");
-          logged = true;
-     }
+    static bool logged = false;
+    if (!logged) {
+        Logger::Log("[HOOK] GhostInfo.Update is executing.");
+        logged = true;
+    }
 
-     if (Features::bGhostTypeEnabled && instance && !IsBadReadPtr(instance, 0x200)) {
-        static std::string lastGhost = "";
-          void** fields = (void**)instance;
+    if (Features::bGhostTypeEnabled && instance && !IsBadReadPtr(instance, 0x200)) {
+    static std::string lastGhost = "";
+        void** fields = (void**)instance;
 
-          // Scan memory for the ghost's name string
-        for (int i = 0; i < 100; i++) {
-            std::string currentName = SDK::IL2CPP_To_String(fields[i]);
-            if (!currentName.empty()) {
-                // Find ghost type ID (usually an integer between 0-27)
-                int typeIdx = 0;
-                int* intFields = (int*)instance;
-                for (int j = 0; j < 64; j++) {
-                    if (intFields[j] > 0 && intFields[j] <= 29) {
-                        typeIdx = intFields[j];
-                        break;
-                    }
+        // Scan memory for the ghost's name string
+    for (int i = 0; i < 100; i++) {
+        std::string currentName = SDK::IL2CPP_To_String(fields[i]);
+        if (!currentName.empty()) {
+            // Find ghost type ID (usually an integer between 0-27)
+            int typeIdx = 0;
+            int* intFields = (int*)instance;
+            for (int j = 0; j < 64; j++) {
+                if (intFields[j] > 0 && intFields[j] <= 29) {
+                    typeIdx = intFields[j];
+                    break;
                 }
-
-                const char* types[] = { "Spirit", "Wraith", "Phantom", "Poltergeist", "Banshee", "Jinn", "Mare", "Revenant", "Shade", "Demon", "Yurei", "Oni", "Yokai", "Hantu", "Goryo", "Myling", "Onryo", "The Twins", "Raiju", "Obake", "Mimic", "Moroi", "Deogen", "Thaye", "None", "Gallu", "Dayan", "Obambo", "Aswang", "Kormos"};
-
-                if (currentName != lastGhost) {
-                    printf("\n[GHOST][ID: %d] %s is a %s\n", typeIdx, currentName.c_str(), types[typeIdx]);
-                    Logger::Log("[GHOST] %s is a %s", currentName.c_str(), types[typeIdx]);
-                    lastGhost = currentName;
-                }
-                break;
             }
+
+            const char* types[] = { "Spirit", "Wraith", "Phantom", "Poltergeist", "Banshee", "Jinn", "Mare", "Revenant", "Shade", "Demon", "Yurei", "Oni", "Yokai", "Hantu", "Goryo", "Myling", "Onryo", "The Twins", "Raiju", "Obake", "Mimic", "Moroi", "Deogen", "Thaye", "None", "Gallu", "Dayan", "Obambo", "Aswang", "Kormos"};
+
+            if (currentName != lastGhost) {
+                printf("\n[GHOST][ID: %d] %s is a %s\n", typeIdx, currentName.c_str(), types[typeIdx]);
+                Logger::Log("[GHOST] %s is a %s", currentName.c_str(), types[typeIdx]);
+                lastGhost = currentName;
+            }
+            break;
         }
-     }
-     oGhostUpdate(instance, methodInfo);
+    }
+    }
+    oGhostUpdate(instance, methodInfo);
 }
